888f616297ac5ff2ca701c856e93d6d364f147bc
KP <kalvin.paviel@epitech.eu>
2024-03-25 12:15:02 +0100

    [+] Exercice 9 fini

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,8 +1,14 @@
-function afficherJoursSemaine() {
-    var joursSemaine = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
-    for (var i = 0; i < joursSemaine.length; i++) {
-        console.log(joursSemaine[i]);
+function tri(numb, order) {
+    if (order === "asc") {
+        return numb.sort((a, b) => a - b);
+    } else if (order === "desc") {
+        return numb.sort((a, b) => b - a);
+    } else {
+        return numb;
     }
 }
 
-afficherJoursSemaine();
+//let arr = [3, 1, 4, 1, 5, 9, 2, 6, 5];
+//console.log("arr", arr);
+//console.log("asc", tri([...arr], "asc"));
+//console.log("desc", tri([...arr], "desc"));
