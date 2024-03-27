6fc304fdaee6eb6b255877c7f844df45c279bfbe
KP <kalvin.paviel@epitech.eu>
2024-03-27 12:31:53 +0100

    [=] Variable

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,12 +1,12 @@
 // Sélection du bouton pour afficher le texte
 const hideButton = document.getElementById('hideButton');
 const showButton = document.getElementById('showButton');
-const textContainer = document.getElementById('textContainer');
+const text = document.getElementById('textContainer');
 
 showButton.addEventListener('click', function() {
-  textContainer.style.display = 'block';
+  text.style.display = 'block';
 });
 
 hideButton.addEventListener('click', function() {
-  textContainer.style.display = 'none';
+  text.style.display = 'none';
 });
